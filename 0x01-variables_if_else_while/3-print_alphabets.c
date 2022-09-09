2937532fab0e27d1632c89f5a5e93d164ac88584
Sammiles07 <samueluche501@gmail.com>
2022-09-09 12:17:17 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char heart;
+
+	for (heart = 'a'; heart <= "z"; heart++)
+		putchar(heart);
+	for (heart = 'A'; heart <= 'Z'; heart++)
+		putchar(heart);
+	putchar('\n');
+	return (0);
+}
