8fd812cfc6ddfa6134cbb6001498adfada30ecbc
Sammiles07 <samueluche501@gmail.com>
2022-09-11 07:43:39 -0700

    task12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,31 +10,7 @@ int main(void)
 {
 	int i, j, k;
 
-	i = 48;
-	j = 48;
+	i = 46;
+	j = 47;
 	k = 48;
-	while (i < 58)
-	{
-		j = i + 1;
-		while (j < 58)
-		{
-			k = j + 1;
-			while (k < 58)
-			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i < 55 || j < 56 || k < 57)
-				{
-					putchar(44);
-					putchar(32);
-					k++;
-				}
-				j++;
-			}
-			i++;
-		}
-	}
-		putchar(10);
-		return (0);
-}
+
