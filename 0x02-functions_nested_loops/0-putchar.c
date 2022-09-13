50e831d657d197845646cc90abd687354c04dab3
Sammiles07 <samueluche501@gmail.com>
2022-09-13 11:18:15 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check Description
+ *
+ * Description: write program that prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char nm = "Samuel";
+
+	while (*nm)
+	{
+		_putchar(*nm);
+		nm++;
+		_putchar('\n');
+	}
+	return (0);
+}
+
