304caaa2095480fa02f826089c5ebc9b51c3eb05
Sammiles07 <samueluche501@gmail.com>
2022-09-19 13:36:36 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (*s[i] != '\0')
 	{
 		i++;
 	}
 	i--;
 	while (i >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(*s[i]);
 		i--;
 	}
 	_putchar('\n');
