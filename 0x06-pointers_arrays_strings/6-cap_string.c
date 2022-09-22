3d494d2c654bcc03cd9faf0ec12fb3a040a29a9b
Sammiles07 <samueluche501@gmail.com>
2022-09-22 11:59:08 -0700

    task0

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,7 @@ char *cap_string(char *s)
 	{
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
+
 		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
